ca3030c14361a25a3e1b9dc16b88380fb56db04f
ksc3045 <sharathchandrakanduri@gmail.com>
2025-05-22 07:10:19 +0530

    basic oops

---
+++ oops1.py
@@ -0,0 +1,20 @@
+class Employee:
+    #define attributes
+    #special method/magic method/dunder method - constructor
+    def __init__(self):
+        self.id=123
+        self.salary=50000
+        self.designation="sde"
+    def travel(self, destination):
+        print(f"employee is travelling to {destination}")
+
+
+#create an instance of the class
+sam=Employee()
+print(sam.id)
+print(sam.salary)
+print(sam.designation)
+sam.travel("Hyderabad")
+
+print(type(sam))
+print(3+5)
---
+++ rough.py
@@ -0,0 +1,4 @@
+lst=[2,3,4,5,7]
+str="nee mama barrey"
+print(type(str))
+lst.
